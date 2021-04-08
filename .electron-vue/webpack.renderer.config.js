e5143e989648c20a4d315aa912fb4cdb155403ed
piaoyangde <119113916@qq.com>
2021-04-08 12:49:52 +0800

    EVE-project

--- .electron-vue/webpack.renderer.config.js
+++ .electron-vue/webpack.renderer.config.js
@@ -19,7 +19,8 @@ const { VueLoaderPlugin } = require('vue-loader')
  * that provide pure *.vue files that need compiling
  * https://simulatedgreg.gitbooks.io/electron-vue/content/en/webpack-configurations.html#white-listing-externals
  */
-let whiteListedModules = ['vue']
+//添加白名单，
+let whiteListedModules = ['vue','element-ui']
 
 let rendererConfig = {
   devtool: '#cheap-module-eval-source-map',
--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -1,33 +1,13 @@
 <template>
   <div id="app">
-    <!-- <router-view></router-view> -->
-
-    <button @click="finduser">查询用户</button>
-    <el-input v-model="input" placeholder="请输入内容"></el-input>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-import { UserModel } from "../dbModel/db";
 
 export default {
   name: "electron",
-  methods: {
-    finduser() {
-      UserModel.find((err, res) => {
-        if (err) {
-          alert("查询错误");
-          return;
-        }
-        console.log(res[0]._doc.name, res[0]._doc.phone);
-      });
-    },
-  },
-  data() {
-    return {
-      input: "",
-    };
-  },
 };
 </script>
 
